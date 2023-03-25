3fd3f268430162dc5afab067b257a747545f90e8
Anton P <a.s.panferov@mail.ru>
2023-03-25 18:26:28 +0300

    changed objects of subject

--- src/Shedule/Main.java
+++ src/Shedule/Main.java
@@ -8,20 +8,14 @@ public class Main {
     public static void main(String[] args) {
 
         Group gr_1 = new Group("10A", 100, 4);
-        Sub match = new Sub("Match", 40);
-        Sub history = new Sub("History", 30);
-        Sub language = new Sub("Language", 30);
-        gr_1.addSubject(match);
-        gr_1.addSubject(history);
-        gr_1.addSubject(language);
+        Sub match_1 = new Sub("Match", 40);
+        Sub history_1 = new Sub("History", 30);
+        Sub language_1 = new Sub("Language", 30);
+        gr_1.addSubject(match_1);
+        gr_1.addSubject(history_1);
+        gr_1.addSubject(language_1);
         Group gr_2 = new Group("8 B", 300, 5);
-        match.setCountHours(50);
-        gr_2.addSubject(match);
-        history.setCountHours(40);
-        gr_2.addSubject(history);
-        history.setCountHours(50);
-        gr_2.addSubject(language);
-        gr_1.getAllCurriculum();
         Group gr_3 = new Group("9A", 200, 4);
+        Group gr_4 = new Group("6G", 150, 9);
     }
 }
