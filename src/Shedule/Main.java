69a873d857ee74a2ad31ec413eaa347aec5c103a
Anton P <a.s.panferov@mail.ru>
2023-03-25 18:10:26 +0300

    corrected mistakes

--- src/Shedule/Main.java
+++ src/Shedule/Main.java
@@ -7,14 +7,14 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        Group gr_1 = new Group("10А", 100);
+        Group gr_1 = new Group("10A", 100, 4);
         Sub match = new Sub("Match", 40);
         Sub history = new Sub("History", 30);
         Sub language = new Sub("Language", 30);
         gr_1.addSubject(match);
         gr_1.addSubject(history);
         gr_1.addSubject(language);
-        Group gr_2 = new Group();
+        Group gr_2 = new Group("8 B", 300, 5);
         match.setCountHours(50);
         gr_2.addSubject(match);
         history.setCountHours(40);
